efe45ef6735e7b05ae63248e9d8adcc1e4a2fb27
Lyle Hayhurst <sozinsky@gmail.com>
2014-12-08 13:26:59 -0600

    moving into maintenance mode

--- xwlists.py
+++ xwlists.py
@@ -16,7 +16,7 @@ UPLOAD_FOLDER = "static/tourneys"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 ALLOWED_EXTENSIONS = set( ['png', 'jpg', 'jpeg', 'gif'])
 
-is_maintenance_mode = False
+is_maintenance_mode = True
 
 here = os.path.dirname(__file__)
 static_dir = os.path.join( here, app.config['UPLOAD_FOLDER'] )
