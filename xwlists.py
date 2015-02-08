7f4b38d027aa21bfcd7721b838e91e322a2e7832
Lyle Hayhurst <sozinsky@gmail.com>
2015-02-08 12:51:31 -0600

    added k4

--- xwlists.py
+++ xwlists.py
@@ -106,6 +106,14 @@ def tourneys():
     summary = PersistenceManager(myapp.db_connector).get_tourney_summary()
     return render_template('tourneys.html', tourneys=summary, admin=admin_on )
 
+@app.route("/tourney_results")
+def get_tourney_results():
+    tourney_id   = request.args.get('tourney_id')
+    tourney = PersistenceManager(myapp.db_connector).get_tourney_by_id(tourney_id)
+
+    return redirect(url_for('get_tourney_details', tourney_id=tourney_id))
+
+
 @app.route("/get_tourney_details")
 def get_tourney_details():
     tourney_id   = request.args.get('tourney_id')
