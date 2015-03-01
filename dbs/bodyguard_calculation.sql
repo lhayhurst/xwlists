5e6671bacc5a75434d61bdff8909a94f4f58264a
lhayhurst <sozinsky@gmail.com>
2015-03-01 08:12:24 -0600

    oops, forgot about bodyguard and calculation!

---
+++ dbs/bodyguard_calculation.sql
@@ -0,0 +1,9 @@
+use sozin$lists;
+SET SQL_SAFE_UPDATES = 0;
+insert into upgrade ( upgrade_type, name, cost, canon_name )
+values
+( 'ept', 'Bodyguard', 2, 'bodyguard');
+
+insert into upgrade ( upgrade_type, name, cost, canon_name )
+values
+( 'ept', 'Calculation', 1, 'calculation');
