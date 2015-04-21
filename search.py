84bfcf2caca0d0131d6e211b0ca3815e009bba01
lhayhurst <sozinsky@gmail.com>
2015-04-21 16:01:06 -0500

    basic search

--- search.py
+++ search.py
@@ -1,13 +1,61 @@
 import unittest
-from sqlalchemy import select, or_, and_
+from sqlalchemy import and_
 from sqlalchemy.dialects import mysql
-from sqlalchemy.orm import aliased
 import myapp
 from persistence import Match, Tourney, TourneyVenue, TourneyPlayer, TourneyList, Ship, ShipPilot, Pilot, ShipUpgrade, \
     Upgrade
 
 def wildcard(term):
     return "*" + term + "*"
+
+class Search:
+
+    def __init__(self, tourney_name=None, locale_name=None, player_name=None, ship_name=None, pilot_name=None, upgrade_name=None):
+        self.tourney_name = tourney_name
+        self.locale_name = locale_name
+        self.player_name = player_name
+        self.ship_name = ship_name
+        self.pilot_name = pilot_name
+        self.upgrade_name = upgrade_name
+
+    def search(self):
+
+        myapp.db_connector.connect()
+        session = myapp.db_connector.get_session()
+
+        and_clauses = []
+        if self.tourney_name is not None:
+             and_clauses.append( Match([Tourney.tourney_name, Tourney.tourney_type], wildcard(self.tourney_name) ) )
+        if self.locale_name is not None:
+            and_clauses.append( Match([TourneyVenue.country, TourneyVenue.state, TourneyVenue.city, TourneyVenue.venue ], wildcard( self.locale_name ) ))
+        if self.player_name is not None:
+            and_clauses.append( Match([TourneyPlayer.player_name], wildcard( self.player_name ) ))
+        if self.ship_name is not None:
+            and_clauses.append( Match([ShipPilot.ship_type], wildcard( self.ship_name ) ) )
+        if self.pilot_name is not None:
+            and_clauses.append( Match([Pilot.name, Pilot.canon_name], wildcard(self.pilot_name) ))
+        if self.upgrade_name is not None:
+            and_clauses.append( Match([Upgrade.name, Upgrade.canon_name], wildcard(self.upgrade_name ) ) )
+
+
+        match = session.query( Tourney, TourneyPlayer, TourneyList, Ship, ShipPilot, Pilot, ShipUpgrade, Upgrade).\
+            join( TourneyVenue).\
+            join( TourneyPlayer).\
+            join( TourneyList).\
+            join( Ship).\
+            join( ShipPilot).\
+            join( Pilot ).\
+            outerjoin( ShipUpgrade).\
+            outerjoin( Upgrade).\
+            filter( and_( *and_clauses ) ).\
+            statement.compile(dialect=mysql.dialect())
+
+        connection = myapp.db_connector.get_engine().connect()
+        ret = connection.execute(match)
+
+        return ret
+
+
 
 class SearchTest(unittest.TestCase):
 
@@ -17,36 +65,14 @@ class SearchTest(unittest.TestCase):
         myapp.db_connector.connect()
         session = myapp.db_connector.get_session()
 
-        tourney = "world"
-        city    = "roseville"
-        player  = "dom"
-        ship    = "tie"
-        pilot   = "soontir"
+        mysearch = Search( tourney_name="world",  locale_name="roseville",
+                           player_name="dom", ship_name="tie", pilot_name="soontir", upgrade_name="target")
 
-        match = session.query( Tourney, TourneyList, TourneyPlayer, Ship, ShipPilot, Pilot, ShipUpgrade, Upgrade ).\
-            join( Tourney, TourneyVenue).\
-            join( Tourney, TourneyPlayer).\
-            join( Tourney, TourneyList).\
-            join( TourneyPlayer, TourneyList ).\
-            join( Ship, TourneyList ).\
-            join( ShipPilot, Ship ).\
-            join( Pilot, ShipPilot ).\
-            outerjoin( ShipUpgrade, Ship).\
-            outerjoin( Upgrade, ShipUpgrade).\
-            filter( and_(
-                         Match([Tourney.tourney_name, Tourney.tourney_type], wildcard(tourney) ),
-                         Match([TourneyVenue.country, TourneyVenue.state, TourneyVenue.city, TourneyVenue.venue ], wildcard( city ) ),
-                         Match([TourneyPlayer.player_name], wildcard( player ) ),
-                         Match([ShipPilot.ship_type], wildcard( ship ) ),
-                         Match([Pilot.name, Pilot.canon_name], wildcard(pilot) ),
-                   ) ).\
-            statement.compile(dialect=mysql.dialect())
+        ret = mysearch.search()
 
-        connection = myapp.db_connector.get_engine().connect()
-        ret = connection.execute(match)
-        for row in ret:
-            print row
-
+        self.assertTrue( ret.rowcount == 1)
+        result = [r[0] for r in ret]
+        print result
 
 if __name__ == "__main__":
     unittest.main()
