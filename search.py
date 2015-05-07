283f5a5ea9e75112c37a8914a2c9602136963987
lhayhurst <sozinsky@gmail.com>
2015-05-06 19:08:22 -0500

    added geography searching

--- search.py
+++ search.py
@@ -7,7 +7,7 @@ from whoosh.query import Term
 
 import myapp
 from persistence import TourneyList, Ship, ShipPilot, Pilot, ShipUpgrade, \
-    Upgrade, PersistenceManager
+    Upgrade, PersistenceManager, Tourney, TourneyVenue
 
 
 def wildcard(term):
@@ -22,12 +22,21 @@ expression_map = { 'AND' : and_,
 PILOT_MATCH = "pilot_match"
 SHIP_MATCH  = "ship_match"
 UPGRADE_MATCH = "upgrade_match"
+COUNTRY_MATCH = "country_match"
+STATE_MATCH   = "state_match"
+CITY_MATCH    = "city_match"
+VENUE_MATCH   = "venue_match"
+
 term_type_map = { "p": PILOT_MATCH,
                   "pilot": PILOT_MATCH,
                   "s" : SHIP_MATCH,
                   "ship" : SHIP_MATCH,
                   "u" : UPGRADE_MATCH,
-                  "upgrade" : UPGRADE_MATCH
+                  "upgrade" : UPGRADE_MATCH,
+                  "country" : COUNTRY_MATCH,
+                  "state" : STATE_MATCH,
+                  "city" : CITY_MATCH,
+                  "venue" : VENUE_MATCH
                 }
 
 def tree_to_expr(tree, subq):
@@ -41,6 +50,15 @@ def tree_to_expr(tree, subq):
             return subq.c.ship_name.like( '%' + term_value + '%')
         elif match_expr == UPGRADE_MATCH:
             return subq.c.upgrade_name.like( '%' + term_value + '%')
+        elif match_expr == COUNTRY_MATCH:
+            return subq.c.country_name.like( '%' + term_value + '%')
+        elif match_expr == STATE_MATCH:
+            return subq.c.state_name.like( '%' + term_value + '%')
+        elif match_expr == CITY_MATCH:
+            return subq.c.city_name.like( '%' + term_value + '%')
+        elif match_expr == VENUE_MATCH:
+            return subq.c.venue_name.like( '%' + term_value + '%')
+
     fn = expression_map[tree.JOINT.strip()]
     return fn(
         *(
@@ -48,7 +66,7 @@ def tree_to_expr(tree, subq):
         )
     )
 
-kvpair = re.compile( r'^(s=.+)|(ship=.+)|(p=.+)|(pilot=.+)|(u=.+)|(upgrade=.+)')
+kvpair = re.compile( r'^(s=.+)|(ship=.+)|(p=.+)|(pilot=.+)|(u=.+)|(country=.+)|(state=.+)|(city=.+)|(venue=.+)')
 
 class Search:
 
@@ -86,9 +104,15 @@ class Search:
         session = myapp.db_connector.get_session()
 
         subq = session.query( TourneyList.id.label("tourney_list_id"),
+                              TourneyVenue.country.label("country_name"),
+                              TourneyVenue.state.label("state_name"),
+                              TourneyVenue.city.label("city_name"),
+                              TourneyVenue.venue.label("venue_name"),
                               func.group_concat( ShipPilot.ship_type.distinct()).label("ship_name" ),
                               func.group_concat( func.concat( Pilot.name, " ", Pilot.canon_name )).label("pilot_name"),
                               func.group_concat( func.concat( Upgrade.name, " ", Upgrade.canon_name ) ).label("upgrade_name") ). \
+            join(Tourney).\
+            join(TourneyVenue).\
             join(Ship). \
             join(ShipPilot). \
             join(Pilot). \
