0edcd6d3b21a488413a76782f5fdfc9e54cff093
lhayhurst <sozinsky@gmail.com>
2015-05-18 18:25:33 -0500

    added endpoint for mysql

--- xwlists.py
+++ xwlists.py
@@ -710,7 +710,7 @@ endpoints = { VOIDSTATE : 'http://xwing-builder.co.uk/import',
 #     return response.text
 
 
-@app.route("/mysqldb")
+@app.route("/mysqldb", methods=['GET'])
 def get_mysql_dump_file():
     return send_from_directory( directory="dbs", filename="prod.sql")
 
