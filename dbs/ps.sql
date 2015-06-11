09f536ad063b1bc759fc7663a60af5ec17ba0d25
lhayhurst <sozinsky@gmail.com>
2015-06-10 23:11:24 -0500

    pilot skill

---
+++ dbs/ps.sql
@@ -0,0 +1,133 @@
+ALTER TABLE `sozin$lists`.`pilot`
+ADD COLUMN `pilot_skill` TINYINT(1) NULL AFTER `canon_name`;
+
+
+update pilot set pilot_skill=8 where canon_name='carnorjax';
+update pilot set pilot_skill=3 where canon_name='greensquadronpilot';
+update pilot set pilot_skill=8 where canon_name='janors';
+update pilot set pilot_skill=7 where canon_name='landocalrissian';
+update pilot set pilot_skill=1 where canon_name='academypilot';
+update pilot set pilot_skill=4 where canon_name='redsquadronpilot';
+update pilot set pilot_skill=3 where canon_name='obsidiansquadronpilot';
+update pilot set pilot_skill=4 where canon_name='graysquadronpilot';
+update pilot set pilot_skill=4 where canon_name='sabersquadronpilot';
+update pilot set pilot_skill=6 where canon_name='ibtisam';
+update pilot set pilot_skill=6 where canon_name='royalguardpilot';
+update pilot set pilot_skill=6 where canon_name='kylekatarn';
+update pilot set pilot_skill=4 where canon_name='roarkgarnet';
+update pilot set pilot_skill=7 where canon_name='majorrhymer';
+update pilot set pilot_skill=7 where canon_name='keyanfarlander';
+update pilot set pilot_skill=4 where canon_name='captainyorr';
+update pilot set pilot_skill=5 where canon_name='krassistrelix';
+update pilot set pilot_skill=6 where canon_name='echo';
+update pilot set pilot_skill=5 where canon_name='nightbeast';
+update pilot set pilot_skill=5 where canon_name='etahnabaht';
+update pilot set pilot_skill=9 where canon_name='hansolo';
+update pilot set pilot_skill=8 where canon_name='airencracken';
+update pilot set pilot_skill=6 where canon_name='lieutenantblount';
+update pilot set pilot_skill=8 where canon_name='bobafett';
+update pilot set pilot_skill=7 where canon_name='kathscarlet';
+update pilot set pilot_skill=7 where canon_name='maulermithel';
+update pilot set pilot_skill=5 where canon_name='felswrath';
+update pilot set pilot_skill=4 where canon_name='daggersquadronpilot';
+update pilot set pilot_skill=5 where canon_name='biggsdarklighter';
+update pilot set pilot_skill=6 where canon_name='colonelvessery';
+update pilot set pilot_skill=3 where canon_name='patrolleader';
+update pilot set pilot_skill=3 where canon_name='bountyhunter';
+update pilot set pilot_skill=3 where canon_name='eadenvrill';
+update pilot set pilot_skill=5 where canon_name='chewbacca';
+update pilot set pilot_skill=2 where canon_name='goldsquadronpilot';
+update pilot set pilot_skill=1 where canon_name='deltasquadronpilot';
+update pilot set pilot_skill=4 where canon_name='blacksquadronpilot';
+update pilot set pilot_skill=6 where canon_name='backstabber';
+update pilot set pilot_skill=6 where canon_name='coloneljendon';
+update pilot set pilot_skill=8 where canon_name='rearadmiralchiraneau';
+update pilot set pilot_skill=8 where canon_name='corranhorn';
+update pilot set pilot_skill=7 where canon_name='maarekstele';
+update pilot set pilot_skill=5 where canon_name='hobbieklivian';
+update pilot set pilot_skill=7 where canon_name='tetrancowall';
+update pilot set pilot_skill=2 where canon_name='banditsquadronpilot';
+update pilot set pilot_skill=6 where canon_name='garvendreis';
+update pilot set pilot_skill=3 where canon_name='sigmasquadronpilot';
+update pilot set pilot_skill=8 where canon_name='rexlerbrath';
+update pilot set pilot_skill=8 where canon_name='hortonsalm';
+update pilot set pilot_skill=2 where canon_name='bluesquadronpilot';
+update pilot set pilot_skill=1 where canon_name='alphasquadronpilot';
+update pilot set pilot_skill=4 where canon_name='captainoicunn';
+update pilot set pilot_skill=7 where canon_name='jekporkins';
+update pilot set pilot_skill=2 where canon_name='omicrongrouppilot';
+update pilot set pilot_skill=5 where canon_name='ltlorrir';
+update pilot set pilot_skill=3 where canon_name='blackmoonsquadronpilot';
+update pilot set pilot_skill=5 where canon_name='gemmersojan';
+update pilot set pilot_skill=4 where canon_name='gammasquadronpilot';
+update pilot set pilot_skill=1 where canon_name='prototypepilot';
+update pilot set pilot_skill=5 where canon_name='leebo';
+update pilot set pilot_skill=8 where canon_name='tychocelchu';
+update pilot set pilot_skill=9 where canon_name='darthvader';
+update pilot set pilot_skill=8 where canon_name='howlrunner';
+update pilot set pilot_skill=8 where canon_name='captainkagi';
+update pilot set pilot_skill=7 where canon_name='whisper';
+update pilot set pilot_skill=6 where canon_name='commanderkenkirk';
+update pilot set pilot_skill=2 where canon_name='wildspacefringer';
+update pilot set pilot_skill=6 where canon_name='darkcurse';
+update pilot set pilot_skill=1 where canon_name='outerrimsmuggler';
+update pilot set pilot_skill=8 where canon_name='tennumb';
+update pilot set pilot_skill=8 where canon_name='soontirfel';
+update pilot set pilot_skill=6 where canon_name='arvelcrynyd';
+update pilot set pilot_skill=3 where canon_name='onyxsquadronpilot';
+update pilot set pilot_skill=3 where canon_name='avengersquadronpilot';
+update pilot set pilot_skill=2 where canon_name='rookiepilot';
+update pilot set pilot_skill=5 where canon_name='wingedgundark';
+update pilot set pilot_skill=8 where canon_name='wesjanson';
+update pilot set pilot_skill=8 where canon_name='lukeskywalker';
+update pilot set pilot_skill=6 where canon_name='captainjonus';
+update pilot set pilot_skill=3 where canon_name='tarnmison';
+update pilot set pilot_skill=4 where canon_name='talasquadronpilot';
+update pilot set pilot_skill=5 where canon_name='neradantels';
+update pilot set pilot_skill=2 where canon_name='rebeloperative';
+update pilot set pilot_skill=7 where canon_name='jakefarrell';
+update pilot set pilot_skill=6 where canon_name='kirkanos';
+update pilot set pilot_skill=7 where canon_name='dashrendar';
+update pilot set pilot_skill=9 where canon_name='wedgeantilles';
+update pilot set pilot_skill=1 where canon_name='knavesquadronpilot';
+update pilot set pilot_skill=2 where canon_name='scimitarsquadronpilot';
+update pilot set pilot_skill=6 where canon_name='dutchvander';
+update pilot set pilot_skill=7 where canon_name='turrphennir';
+update pilot set pilot_skill=2 where canon_name='tempestsquadronpilot';
+update pilot set pilot_skill=6 where canon_name='ig88d';
+update pilot set pilot_skill=2 where canon_name='syndicatethug';
+update pilot set pilot_skill=5 where canon_name='drearenthal';
+update pilot set pilot_skill=7 where canon_name='kavil';
+update pilot set pilot_skill=5 where canon_name='mandalorianmercenary';
+update pilot set pilot_skill=6 where canon_name='emonazzameen';
+update pilot set pilot_skill=3 where canon_name='torkilmux';
+update pilot set pilot_skill=7 where canon_name='dacebonearm';
+update pilot set pilot_skill=5 where canon_name='palobgodalhi';
+update pilot set pilot_skill=1 where canon_name='binayrepirate';
+update pilot set pilot_skill=3 where canon_name='blacksunsoldier';
+update pilot set pilot_skill=5 where canon_name='kaatoleeachos';
+update pilot set pilot_skill=7 where canon_name='ndrusuhlak';
+update pilot set pilot_skill=1 where canon_name='blacksunenforcer';
+update pilot set pilot_skill=3 where canon_name='blacksunvigo';
+update pilot set pilot_skill=5 where canon_name='guri';
+update pilot set pilot_skill=7 where canon_name='princexizor';
+update pilot set pilot_skill=6 where canon_name='ig88a';
+update pilot set pilot_skill=6 where canon_name='ig88b';
+update pilot set pilot_skill=6 where canon_name='ig88c';
+update pilot set pilot_skill=1 where canon_name='spicerunner';
+update pilot set pilot_skill=2 where canon_name='cartelspacer';
+update pilot set pilot_skill=5 where canon_name='tansariipointveteran';
+update pilot set pilot_skill=6 where canon_name='laetinashera';
+update pilot set pilot_skill=8 where canon_name='serissu';
+update pilot set pilot_skill=4 where canon_name='hiredgun';
+
+
+
+
+
+
+
+
+
+
+
