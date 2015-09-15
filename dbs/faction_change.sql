78810c46310b282f80b7badf586be05e14c41d0f
lhayhurst <sozinsky@gmail.com>
2015-09-15 16:49:10 -0500

    xws 0.3.0 (new factions)

--- dbs/faction_change.sql
+++ dbs/faction_change.sql
@@ -1,13 +1,15 @@
+
 SET SQL_SAFE_UPDATES = 0;
 
 ALTER TABLE `sozin$lists`.`tourney_list`
 CHANGE COLUMN `faction` `faction` ENUM('rebels','empire', 'scum', 'rebel', 'imperial') NULL DEFAULT NULL ;
 
-UPDATE TABLE `sozin$lists`.`tourney_list`
+
+UPDATE `sozin$lists`.`tourney_list`
 set `faction` = 'imperial' where
 `faction` = 'empire';
 
-UPDATE TABLE `sozin$lists`.`tourney_list`
+UPDATE `sozin$lists`.`tourney_list`
 set `faction` = 'rebel' where
 `faction` = 'rebels';
 
