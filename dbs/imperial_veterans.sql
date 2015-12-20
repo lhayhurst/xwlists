0c457c4609c69054ec4bf198fbe1a9c219d3adac
Lyle Hayhurst <sozinsky@gmail.com>
2015-12-20 13:04:23 -0600

    imperial vets

---
+++ dbs/imperial_veterans.sql
@@ -0,0 +1,15 @@
+insert into upgrade ( upgrade_type, name, cost, canon_name )
+values
+( 'title', 'TIE/x7', -2, 'tiex7');
+
+insert into upgrade ( upgrade_type, name, cost, canon_name )
+values
+( 'title', 'TIE/D', 0, 'tied');
+
+insert into upgrade ( upgrade_type, name, cost, canon_name )
+values
+( 'title', 'Tie Shuttle', 0, 'tieshuttle');
+
+insert into upgrade ( upgrade_type, name, cost, canon_name )
+values
+( 'cannon', 'Tractor Beam', 1, 'tractorbeam');
