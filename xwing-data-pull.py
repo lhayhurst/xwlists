28a7e3cde2e2d23f47ce79995075514f9b557e99
Lyle Hayhurst <sozinsky@gmail.com>
2017-08-21 10:57:47 -0700

    fixing a bunch of names

--- xwing-data-pull.py
+++ xwing-data-pull.py
@@ -18,9 +18,20 @@ if __name__ == '__main__':
         ships = json.loads(data_file.read())
         for ship in ships:
             ship_map[ship["name"]] = ship
+            #todo: implement new ships
 
 
     session = myapp.db_connector.get_session()
+
+    #listjuggler doesn't support epic
+    pilot_exclusions = ( "GR-75 Medium Transport", \
+                   "CR90 Corvette (Fore)", \
+                   "CR90 Corvette (Aft)", \
+                   "Raider-class Corvette (Fore)", \
+                   "Raider-class Corvette (Aft)",\
+                   "Nashtah Pup Pilot", \
+                   "Gozanti-Class Cruiser", \
+                   "C-ROC Cruiser", )
 
     with open(pilots_file) as data_file:
         pilots = json.loads(data_file.read())
@@ -28,9 +39,19 @@ if __name__ == '__main__':
             p = pilot['name']
             db_pilot = session.query(Pilot).filter(Pilot.name ==p).first()
             if db_pilot is None:
-                db_pilot = session.query(Pilot).filter(Pilot.name ==p).first()
-                if db_pilot is None:
-                    print pilot['name']
+                if not pilot['name'] in pilot_exclusions:
+                    #print pilot['name']
+                    sql = "insert into pilot ( name, canon_name, cost, pilot_skill  ) " \
+                          "values ( '%s','%s', '%d', '%d' );" % \
+                          (pilot['name'], pilot['xws'], pilot['points'], pilot['skill'])
+                    print sql
+                    sql = "insert  ship_pilot ( ship_type, pilot_id) " \
+                          "select '%s', id from pilot where name='%s' and canon_name='%s';" % \
+                          (ship_map[ pilot['ship']]['xws'],
+                           pilot['name'],
+                           pilot['xws'])
+                    print sql
+
 
 
 #    with open(upgrades_file) as data_file:
